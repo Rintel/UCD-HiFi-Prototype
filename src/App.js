312c3597942bda2ff8b60af9fa42e6ad3d8372c4
Cedric Laier <ced.laier@gmail.com>
2019-06-09 19:24:50 +0200

    [feat] Initial project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* Right now its a screenshot of Eclipse in Dark Mode with Saros installed */} 
+      <div className="IDEContainer">
+      </div>
+    </div>
+  );
+}
+
+export default App;
